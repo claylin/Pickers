79daa8620d64a6cee9f74590e0654e2e3fb755e0
claylin <claylin.007@gmail.com>
2015-04-08 16:22:47 +0800

    Custom picker without sounds

--- Pickers/CustomPickerViewController.swift
+++ Pickers/CustomPickerViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import AudioToolbox
 
-class CustomPickerViewController: UIViewController {
+class CustomPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    private var images:[UIImage]!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    private var winSoundID: SystemSoundID = 0
+    private var crunchSoundID: SystemSoundID = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        images = [
+            UIImage(named: "seven")!,
+            UIImage(named: "bar")!,
+            UIImage(named: "crown")!,
+            UIImage(named: "cherry")!,
+            UIImage(named: "lemon")!,
+            UIImage(named: "apple")!
+        ]
+        winLabel.text = " " // Note the space between the quotes
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +37,47 @@ class CustomPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Picker Data Source Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 5
     }
-    */
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
 
+    // MARK: - Picker View Delegate Methods
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+            let image = images[row]
+            let imageView = UIImageView(image: image)
+            return imageView
+    }
+    
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 64 }
+    
+    @IBAction func spin(sender: AnyObject) {
+        var win = false
+        var numInRow = -1
+        var lastVal = -1
+        for i in 0..<5 {
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if newValue == lastVal {
+                numInRow++
+            } else {
+                numInRow = 1
+            }
+            lastVal = newValue
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+            if numInRow >= 3 {
+                win = true
+            }
+        }
+        winLabel.text = win ? "WINNER!" : " "
+    }
+
+    func showButton() {
+        button.hidden = false
+    }
 }
