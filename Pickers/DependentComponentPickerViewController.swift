64eeb6e6991f03966f4f267fd170cf82e603577e
claylin <claylin.007@gmail.com>
2015-04-08 15:03:22 +0800

    Dependent picker works

--- Pickers/DependentComponentPickerViewController.swift
+++ Pickers/DependentComponentPickerViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class DependentComponentPickerViewController: UIViewController {
+class DependentComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var dependentPicker: UIPickerView!
+    
+    private let stateComponent = 0
+    private let zipComponent = 1
+    private var stateZips:[String : [String]]!
+    private var states:[String]!
+    private var zips:[String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let bundle = NSBundle.mainBundle()
+        let plistURL = bundle.URLForResource("statedictionary", withExtension: "plist")
+        stateZips = NSDictionary(contentsOfURL: plistURL!) as [String : [String]]
+        let allStates = stateZips.keys
+        states = sorted(allStates)
+        let selectedState = states[0]
+        zips = stateZips[selectedState]
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +35,53 @@ class DependentComponentPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Picker Data Source Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
     }
-    */
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == stateComponent {
+            return states.count
+        } else {
+            return zips.count
+        }
+    }
 
+    // MARK: - Picker View Delegate Methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if component == stateComponent {
+            return states[row]
+        } else {
+            return zips[row]
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == stateComponent {
+            let selectedState = states[row]
+            zips = stateZips[selectedState]
+            dependentPicker.reloadComponent(zipComponent)
+            dependentPicker.selectRow(0, inComponent: zipComponent, animated: true)
+        }
+    }
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let stateRow = dependentPicker.selectedRowInComponent(stateComponent)
+        let zipRow = dependentPicker.selectedRowInComponent(zipComponent)
+        let state = states[stateRow]
+        let zip = zips[zipRow]
+        let title = "You selected zip code \(zip)"
+        let message = "\(zip) is in \(state)"
+        let alert = UIAlertController(
+                title: title,
+                message: message,
+                preferredStyle: .Alert)
+        let action = UIAlertAction(
+                title: "OK",
+                style: .Default,
+                handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
--- Pickers/ViewController.swift
+++ Pickers/ViewController.swift
@@ -11,9 +11,8 @@ import UIKit
 class ViewController: UITabBarController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        super.viewDidLoad()        
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
